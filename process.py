05ee3f4875870bb3154b62b14e856eec39d8c292
Chris Lowe <csl619@outlook.com>
2021-08-20 15:18:14 +0100

    add unique property id to data lines

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import apache_beam as beam
 from apache_beam.io import ReadFromText, WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 from datetime import datetime
+from hashlib import md5
 
 
 class RemoveQuotations(beam.DoFn):
@@ -14,6 +15,21 @@ class RemoveQuotations(beam.DoFn):
         return [
             element
         ]
+
+
+class AddPropertyId(beam.DoFn):
+    def process(self, element):
+        # start hash algorithm.
+        hash = md5()
+        # create a hash object using the array items for paon, saon, street and postcode.
+        hash.update(
+            f'{element[7]}{element[8]}{element[9]}{element[3]}'.encode('utf-8'))
+        # convert to digest and insert into the element array at position zero.
+        element.insert(0, hash.hexdigest())
+        return [
+            element
+        ]
+
 
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
@@ -40,6 +56,7 @@ def run(argv=None, save_main_session=True):
             lines
             | 'Split lines to arrays' >> beam.Regex.split(regex, outputEmpty=True)
             | 'Remove double quotes from array items' >> beam.ParDo(RemoveQuotations())
+            | 'Add unique identifier for each property' >> beam.ParDo(AddPropertyId())
         )
 
 if __name__ == '__main__':
